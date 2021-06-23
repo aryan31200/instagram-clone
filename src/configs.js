4902f2fce633f5e01bbcf9bbd2b85d39058b0de3
Aryan Gupta <aryan678gupta@gmail.com>
2021-06-23 20:27:07 +0530

    added config to complete checks

---
+++ src/configs.js
@@ -0,0 +1,13 @@
+const configs = {
+  apiKey: "AIzaSyD88NZ_sBWq225NWzkyKwDcGVnOSj76Fa4",
+  authDomain: "react-instagram-clone-c385c.firebaseapp.com",
+  databaseURL:
+    "https://react-instagram-clone-c385c-default-rtdb.firebaseio.com",
+  projectId: "react-instagram-clone-c385c",
+  storageBucket: "react-instagram-clone-c385c.appspot.com",
+  messagingSenderId: "587589930168",
+  appId: "1:587589930168:web:36ac1fb3bf4e3972b6f84d",
+  measurementId: "G-SPC72RV5QW",
+};
+
+export default configs;
